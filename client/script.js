8ea491162d8e1c14b896ed208a958761a1f8ef2c
SAHIL SHARMA <76551267+KIET7UKE@users.noreply.github.com>
2022-12-29 01:14:59 +0530

    changed the fetch path

--- client/script.js
+++ client/script.js
@@ -67,7 +67,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch("https://jsm-openai-codex.onrender.com", {
+  const response = await fetch("http://localhost:5000", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
